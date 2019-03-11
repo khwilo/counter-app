f1c3e5a9f0b7d245be2251bd15aad0876127eb3a
khwilo <khwilowatai@gmail.com>
2019-03-11 16:30:27 +0300

    Bind the count value

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -6,8 +6,13 @@ class Counter extends Component {
     tags: ["tag1", "tag2", "tag3"]
   };
 
+  constructor() {
+    super();
+    this.handleIncrement = this.handleIncrement.bind(this.state.count);
+  }
+
   handleIncrement() {
-    console.log("Increment clicked");
+    console.log("Increment clicked", this);
   }
 
   render() {
