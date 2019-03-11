606d5162f836bb9881515a145708c39e8c7f0681
khwilo <khwilowatai@gmail.com>
2019-03-11 15:38:20 +0300

    Use a class styles property to set the style for an element

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -5,18 +5,25 @@ class Counter extends Component {
     count: 0
   };
 
+  styles = {
+    fontSize: 10,
+    fontWeight: "bold"
+  };
+
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <span style={this.styles} className="badge badge-primary m-2">
+          {this.formatCount()}
+        </span>
         <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
   }
 
   formatCount() {
-      const { count } = this.state;
-      return count === 0 ? 'Zero' : count;
+    const { count } = this.state;
+    return count === 0 ? "Zero" : count;
   }
 }
 
